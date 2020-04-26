c3fea82ceba9462148c8a6ac3825a102f2cbd8d5
Kelly online <kellyonline2016@gmail.com>
2020-04-26 18:59:05 +0100

    create the show error function

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
 
-// const showError = (input, message) => {
+const showError = (input, message) => {
+    const formControl = input.parentElement;
 
-// }
+    formControl.className = 'form-control error';
+
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+}
 form.addEventListener('submit', e => {
     e.preventDefault();
     // console.log('submitted');
