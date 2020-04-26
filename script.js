7ee19381b65d2ca118120fef26df6d426b9353b7
Kelly online <kellyonline2016@gmail.com>
2020-04-26 19:24:00 +0100

    set valid email function

--- script.js
+++ script.js
@@ -21,6 +21,11 @@ const showSuccess = (input) => {
     formControl.className = 'form-control success';    
 }
 
+const isEmailValid = email => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 form.addEventListener('submit', e => {
     e.preventDefault();
     // console.log('submitted');
