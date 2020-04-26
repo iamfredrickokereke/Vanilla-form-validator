5d0511ba1189cd441283b39d77bdd95c7d2837df
Kelly online <kellyonline2016@gmail.com>
2020-04-26 18:46:35 +0100

    add event listeners to form

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const username = document.getElementById('username');
 const email= document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+
+
+// const showError = (input, message) => {
+
+// }
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    // console.log('submitted');
+
+    if (username.value === '') {
+        showError(username, 'username is required!')
+    } else{
+        showSuccess(username)
+    }
+    
+})
