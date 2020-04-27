77d84a13edf46a4808ea8d66e32e190db2b451cb
Kelly online <kellyonline2016@gmail.com>
2020-04-27 23:35:17 +0100

    [Refactor] - update function email parameter for consistent name

--- v2/script.js
+++ v2/script.js
@@ -21,7 +21,7 @@ const showSuccess = (input) => {
     formControl.className = 'form-control success';    
 }
 
-const checkEmail = email => {
+const checkEmail = input => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
