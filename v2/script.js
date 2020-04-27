dfd1194d21febae9307f55bc7a4430ee360e0e6e
Kelly online <kellyonline2016@gmail.com>
2020-04-27 21:41:55 +0100

    Refactor - removed if statements due to repetition

--- v2/script.js
+++ v2/script.js
@@ -26,34 +26,12 @@ const isEmailValid = email => {
     return re.test(String(email).toLowerCase());
 }
 
+
+// check required fields for the inputs
 form.addEventListener('submit', e => {
     e.preventDefault();
     // console.log('submitted');
 
-    if (username.value === '') {
-        showError(username, 'username is required!');
-    }else{
-        showSuccess(username);
-    }
-
-    if (email.value === '') {
-        showError(email, 'email is required!');
-    }else if(!isEmailValid(email.value)){
-        showError(email, 'Email is not valid')
-    } else{
-        showSuccess(email);
-    }
-
-    if (password.value === '') {
-        showError(password, 'password is required!');
-    } else{
-        showSuccess(password);
-    }
-
-    if (password2.value === '') {
-        showError(password2, 'password 2 is required!');
-    } else{
-        showSuccess(password2);
-    }
+    checkRequiredInput()
     
 })
