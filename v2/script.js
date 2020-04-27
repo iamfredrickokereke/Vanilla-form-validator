0965beaafaa8e60a52b986e82fbe8142953b420d
Kelly online <kellyonline2016@gmail.com>
2020-04-27 23:39:17 +0100

    [Refactor] - show error if email is invalid

--- v2/script.js
+++ v2/script.js
@@ -25,6 +25,8 @@ const checkEmail = input => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(input.value.trim())) {
         showSuccess(input);
+    } else{
+        showError(input, `Email is not valid.`)
     }
 }
 
